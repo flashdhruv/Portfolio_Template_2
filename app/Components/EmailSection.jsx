3c3aab31b86a729353654f1ea91d56c9647ff9e5
Dhruv Bahl <bhldhruv@gmail.com>
2023-12-30 20:57:49 -0800

    fixed a couple issues

--- app/Components/EmailSection.jsx
+++ app/Components/EmailSection.jsx
@@ -8,10 +8,10 @@ const EmailSection = () => {
   return (
     <div className='flex flex-col items-center justify-center h-64'>
             <div className='text-center mb-4 '>
-                <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+                <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
                 <p className='text-[#ADB7BE] max-w-md'>
-                I'm currently looking for new opportunities, my inbox is always open.
-                Whether you have a question or just want to say hi, I'll try my best to get back to you.
+                I&apos;m currently looking for new opportunities, my inbox is always open.
+                Whether you have a question or just want to say hi, I&apos;ll try my best to get back to you.
                 </p>
             </div>
             <div className='socials flex flex-row gap-2'>
--- app/Components/HeroSection.jsx
+++ app/Components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
             <div className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
                 <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold'>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-400'>
-                        Hello, I'm {""}
+                        Hello, I&apos;m {""}
                     </span>
                     
                     <br></br>
